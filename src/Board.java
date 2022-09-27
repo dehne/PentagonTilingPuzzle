22173b5cdcd24cee7527baec77d1b0379efe69c2
dehne <dehne1@comcast.net>
2022-09-26 20:02:08 -0700

    Initial commit

---
+++ src/Board.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Board {
+    char[][] cell = new char[8][6];         // The cells of the board: 8 wide and 6 high
+
+    /****
+     * 
+     * Constructor
+     * 
+     ****/
+    Board(){
+        Arrays.fill(cell, ' ');
+    }
+
+}
